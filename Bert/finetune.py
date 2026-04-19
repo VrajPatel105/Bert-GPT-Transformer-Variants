1d46a6ade843176cb9ef4fbc43e43d40cc06a1cc
Vraj Patel <vrajp1052005@gmail.com>
2026-04-19 12:34:26 -0400

    dataloader and lr, optimizer

--- Bert/finetune.py
+++ Bert/finetune.py
@@ -1,8 +1,9 @@
 from datasets import load_dataset
 from transformers import BertForSequenceClassification, BertTokenizer
+from transformers import AdamW
 import torch
 import torch.nn as nn
-from torch.utils.data import dataloader, Dataset
+from torch.utils.data import DataLoader, Dataset
 
 data = load_dataset("glue", "sst2")
 bert = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=2)
@@ -36,9 +37,51 @@ class sst2data(Dataset):
 
     def __getitem__(self, index):
         return self.dataset[index]
-    
+
+# setting all the cols to tensors
+# set_format makes only those columns available when somenoes looks at it. so we already dropped cols like idx, input_ids etc that we did not need
+   
+tokenize_train.set_format("torch", columns=["input_ids", "attention_mask", "label"])
+tokenize_val.set_format("torch", columns=["input_ids", "attention_mask", "label"])
 
 train_ds = sst2data(tokenize_train)
 val_ds = sst2data(tokenize_val)
 
 print(train_ds[0])
+
+# output
+# {'label': tensor(0), 
+# 'input_ids': tensor([  101,  5342,  2047,  3595,  8496,  2013,  1996, 18643,  3197,   102,
+#             0,     0,     0,     0,     0,     0,     0,     0,     0,     0,
+#             0,     0,     0,     0,     0,     0,     0,     0,     0,     0,
+#             0,     0,     0,     0,     0,     0,     0,     0,     0,     0,
+#             0,     0,     0,     0,     0,     0,     0,     0,     0,     0,
+#             0,     0,     0,     0,     0,     0,     0,     0,     0,     0,
+#             0,     0,     0,     0,     0,     0,     0,     0,     0,     0,
+#             0,     0,     0,     0,     0,     0,     0,     0,     0,     0,
+#             0,     0,     0,     0,     0,     0,     0,     0,     0,     0,
+#             0,     0,     0,     0,     0,     0,     0,     0,     0,     0,
+#             0,     0,     0,     0,     0,     0,     0,     0,     0,     0,
+#             0,     0,     0,     0,     0,     0,     0,     0,     0,     0,
+#             0,     0,     0,     0,     0,     0,     0,     0]), 
+# 'attention_mask': tensor([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+#         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+#         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+#         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+#         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+#         0, 0, 0, 0, 0, 0, 0, 0])}
+
+
+# dataloaders
+
+train_data = DataLoader(train_ds, batch_size=32, shuffle=True)
+val_data = DataLoader(val_ds, batch_size=32, shuffle=False)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = bert.to(device)
+
+learning_rate = 2e-5 # 2 x 10^(-5)
+
+optimizer = AdamW(model.parameters(), lr = learning_rate)
+
+epochs = 3
